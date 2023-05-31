2c92d1400da13fc0eeb50cc2092814bf922baddd
JUNGYEOL YANG <101038186+jungyeoly@users.noreply.github.com>
2023-05-31 11:50:52 +0900

    [Bronze V] Title: A+B - 3, Time: 232 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int testcase = scan.nextInt();
+
+		for (int i = 1; i <= testcase; i++) {
+			int A = scan.nextInt();
+			int B = scan.nextInt();
+			System.out.println(A + B);
+		}
+
+		scan.close();
+	}
+}
