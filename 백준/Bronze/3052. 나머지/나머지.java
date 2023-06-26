550ce9fcf8f05b51b0b56f912081eaef74805807
JUNGYEOL YANG <101038186+jungyeoly@users.noreply.github.com>
2023-06-26 10:10:25 +0900

    [Bronze II] Title: 나머지, Time: 208 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		HashSet<Integer> h = new HashSet<Integer>();
+
+		for (int i = 0; i < 10; i++) {
+			h.add(scanner.nextInt() % 42);
+		}
+		scanner.close();
+		System.out.println(h.size());
+	}
+}
+
