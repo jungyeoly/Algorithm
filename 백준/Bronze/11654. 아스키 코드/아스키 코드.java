3df73ac300957e95f724165ab36262c12c463091
JUNGYEOL YANG <101038186+jungyeoly@users.noreply.github.com>
2023-06-29 17:25:50 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		char c = scanner.next().charAt(0);
+		
+		int asciiCode = (int) c;
+		System.out.println(asciiCode);
+		scanner.close();
+	}
+}
